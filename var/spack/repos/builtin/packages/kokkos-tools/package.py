abf8b1fb14b8e2961bfa36d09c221c0afc77d425
Jennifer Green <jkgreen@sandia.gov>
2024-08-07 16:34:29 -0600

    Update package.py
    adjust as per vkale's input... still some syntax and stylistic issues to resolve.

--- var/spack/repos/builtin/packages/kokkos-tools/package.py
+++ var/spack/repos/builtin/packages/kokkos-tools/package.py
@@ -5,7 +5,6 @@
 
 from spack.package import *
 
-
 class KokkosTools(CMakePackage):
     """Kokkos Profiling and Debugging Tools"""
 
@@ -13,7 +12,7 @@ class KokkosTools(CMakePackage):
     git = "https://github.com/kokkos/kokkos-tools.git"
 
     license("Apache-2.0 WITH LLVM-exception")
-
+    maintainers("vkale", "jennfshr", "rbbgerber", "dalg24")
     version("develop", branch="develop")
 
     # C++17 standard support requires sufficiently new compilers
@@ -29,61 +28,48 @@ class KokkosTools(CMakePackage):
             description="Enable reuse of compiler and flags based on Kokkos settings")
     variant("single", default=False, description="Build Monolithic KokkosTools library with profilers")
     variant("testing", default=False, description="Enable Testing")
-    variant("trilinos", default=False, description="Use Kokkos from Trilinos")
+    variant("variorum", default=False, description="Enable Variorum")
     variant("vtune", default=False, description="Enable profiling on VTune proprietary counters")
-    # ArborX relies on Kokkos to provide devices, providing one-to-one matching
-    # variants. The only way to disable those devices is to make sure Kokkos
-    # does not provide them.
-    kokkos_backends = {
-        'serial': (True,  "enable Serial backend (default)"),
-        'cuda': (False,  "enable Cuda backend"),
-        'openmp': (False,  "enable OpenMP backend"),
-        'rocm': (False,  "enable HIP backend")
-    }
-
-    for backend in kokkos_backends:
-        deflt, descr = kokkos_backends[backend]
-        variant(backend.lower(), default=deflt, description=descr)
-
-    # Standalone Kokkos
-    depends_on('kokkos@3.1.00:', when='~trilinos', type=("build", "link", "run"))
-
-    for backend in kokkos_backends:
-        depends_on('kokkos+%s' % backend.lower(), when='~trilinos+%s' %
-                   backend.lower(), type=("build", "link", "run"))
-
-    depends_on('kokkos+cuda_lambda', when='~trilinos+cuda', type=("build", "link", "run"))
-    #depends_on('kokkos+cuda', when='~trilinos+cuda')
-    depends_on('kokkos+openmp', when='~trilinos+openmp', type=("build", "link", "run"))
-    depends_on('kokkos+rocm', when='~trilinos+rocm', type=("build", "link", "run"))
-
-    depends_on('cmake@3.16:', type='build')
-    depends_on('trilinos+kokkos@develop', when='+trilinos', type=("build", "link", "run"))
-    depends_on('trilinos+openmp', when='+trilinos+openmp', type=("build", "link", "run"))
-    conflicts('~serial', when='+trilinos')
-    conflicts('+cuda', when='+trilinos')
+    variant("nvtx", default=False, description="Nvidia profiling")
+    variant("roctx", default=False, description="Rocm profiling")
+    depends_on('cmake@3.16:', type='build', when="kokkos-tools@develop")
     depends_on('intel-oneapi-vtune', when='+vtune', type=("build", "link", "run"))
     depends_on('caliper', when='+caliper', type=("build", "link", "run"))
-    depends_on("kokkos", when="+testing", type=("build", "link", "run"))
-    depends_on("kokkos", when="+examples", type=("build", "link", "run"))
+    depends_on("kokkos@4:", when="+testing", type=("build", "link", "run"))
+    depends_on("kokkos@4:", when="+examples", type=("build", "link", "run"))
+    depends_on("kokkos@4:+cuda", when="+nvtx", type=("build", "link", "run"))
+    depends_on("kokkos@4:+rocm", when="+roctx", type=("build", "link", "run"))
     depends_on('mpi', when='+mpi', type=("build", "link", "run"))
     depends_on('papi@6:', when='+papi', type=("build", "link", "run"))
-    depends_on('variorum', type=("build", "link", "run"))
+    depends_on('variorum', when='+variorum" type=("build", "link", "run"))
 
-    def cmake_args(self):
-        spec = self.spec
-        args = [
-            '-DKokkos_ROOT=%s' % (spec['kokkos'].prefix if '~trilinos' in spec
-                                  else spec['trilinos'].prefix),
-            '-DARBORX_ENABLE_MPI=%s' % ('ON' if '+mpi' in spec else 'OFF'),
-            '-DKokkosTools_ENABLE_APEX=%s' % ('ON' if '+apex' in spec else 'OFF'),
-            '-DKokkosTools_ENABLE_CALIPER=%s' % ('ON' if '+caliper' in spec else 'OFF'),
-            '-DKokkosTools_ENABLE_EXAMPLES=%s' % ('ON' if '+examples' in spec else 'OFF'),
-            '-DKokkosTools_ENABLE_MPI=%s' % ('ON' if '+mpi' in spec else 'OFF'),
-            '-DKokkosTools_ENABLE_PAPI=%s' % ('ON' if '+papi' in spec else 'OFF'),
-            '-DKokkosTools_ENABLE_SINGLE=%s' % ('ON' if '+single' in spec else 'OFF'),
-            '-DKokkosTools_ENABLE_TESTS=%s' % ('ON' if '+tests' in spec else 'OFF')
-        ]
-        if '+cuda' in spec:
-            args.append('-DCMAKE_CXX_COMPILER=%s' % spec["kokkos"].kokkos_cxx)
-        return args
+def build_env(self):
+    if '+nvtx' in spec:
+        setenv Kokkos_ENABLE_CUDA=1
+
+    if '+rocm' in spec:
+        setenv Kokkos_ENABLE_HIP=1
+
+    #if '+vtune' in spec:
+    #    setenv VTUNE_HOME spec['intel-oneapi-vtune'].prefix
+    #    ## Maybe leave this out?
+
+def cmake_args(self):
+    spec = self.spec
+    args = [
+        '-DKokkosTools_ENABLE_MPI=%s' % ('ON' if '+mpi' in spec else 'OFF'),
+        '-DKokkosTools_ENABLE_APEX=%s' % ('ON' if '+apex' in spec else 'OFF'),
+        '-DKokkosTools_ENABLE_CALIPER=%s' % ('ON' if '+caliper' in spec else 'OFF'),
+        '-DKokkosTools_ENABLE_EXAMPLES=%s' % ('ON' if '+examples' in spec else 'OFF'),
+        '-DKokkosTools_ENABLE_PAPI=%s' % ('ON' if '+papi' in spec else 'OFF'),
+        '-DKokkosTools_ENABLE_SINGLE=%s' % ('ON' if '+single' in spec else 'OFF'),
+        '-DKokkosTools_ENABLE_TESTS=%s' % ('ON' if '+tests' in spec else 'OFF')
+    ]
+
+    if '+testing' or '+caliper' or '+examples' or '+nvtx' or '+roctx' or '+vtune' in spec:
+        args.append('-DKokkos_ROOT=%s' % spec['kokkos'].prefix)
+
+    if '+cuda' in spec:
+        args.append('-DCMAKE_CXX_COMPILER=%s' % spec['kokkos'].kokkos_cxx)
+
+    return args
