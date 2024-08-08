035c3fc55c318fde4afe33e1c2c5d7277901ea72
Jennifer Green <jkgreen@sandia.gov>
2024-08-08 10:13:20 -0600

    adding in input from Vivek, dropping trilinos, guarding Kokkos support

--- var/spack/repos/builtin/packages/kokkos-tools/package.py
+++ var/spack/repos/builtin/packages/kokkos-tools/package.py
@@ -10,66 +10,105 @@ class KokkosTools(CMakePackage):
 
     homepage = "https://github.com/kokkos/kokkos-tools/"
     git = "https://github.com/kokkos/kokkos-tools.git"
-
     license("Apache-2.0 WITH LLVM-exception")
     maintainers("vkale", "jennfshr", "rbbgerber", "dalg24")
     version("develop", branch="develop")
 
     # C++17 standard support requires sufficiently new compilers
-    conflicts('%clang@:16')
-    conflicts('%gcc@:9')
+    conflicts("%clang@:16")
+    conflicts("%gcc@:9")
+    
+    # Example requires KokkosTools built with monolothic library interface
+    conflicts("~single", when="+examples")
+    
+    # Kokkos with relevant variants required for nvtx and roctx
+    for connector,variant in ["nvtx", "+cuda"], ["roctx", "+rocm"]:
+        if not (f"^kokkos@4:{variant}"):
+            conflicts(f"+{connector}", msg=f"{connector} requires ^kokkos@4:{variant}")
 
-    variant("apex", default=False, description="Enable APEX support")
-    variant("caliper", default=False, description="Enable Caliper support")
-    variant("examples", default=False, description="Enable examples")
+    variant("apex", default=False, description="Enable building Apex library")
+    variant("caliper", default=False, description="Enable building Caliper library")
+    variant("examples", default=False, description="Build examples")
+    variant("tests", default=False, description="Build tests")
+    variant("reuse-kokkos-compiler", default=False,
+            description="Enable reuse of compiler and flags based on Kokkos settings")
     variant("mpi", default=False, description="Enable MPI support")
     variant("papi", default=False, description="Enable PAPI support")
-    variant("reuse-compiler", default=False,
-            description="Enable reuse of compiler and flags based on Kokkos settings")
-    variant("single", default=False, description="Build Monolithic KokkosTools library with profilers")
-    variant("testing", default=False, description="Enable Testing")
+    variant("single", default=False,
+            description="Build Monolithic KokkosTools library with profilers")
     variant("variorum", default=False, description="Enable Variorum")
-    variant("vtune", default=False, description="Enable profiling on VTune proprietary counters")
+    variant("vtune", default=False,
+            description="Enable profiling on VTune proprietary counters")
     variant("nvtx", default=False, description="Nvidia profiling")
     variant("roctx", default=False, description="Rocm profiling")
-    depends_on('cmake@3.16:', type='build', when="kokkos-tools@develop")
-    depends_on('intel-oneapi-vtune', when='+vtune', type=("build", "link", "run"))
-    depends_on('caliper', when='+caliper', type=("build", "link", "run"))
-    depends_on("kokkos@4:", when="+testing", type=("build", "link", "run"))
+    depends_on("cmake@3.16:", type="build", when="kokkos-tools@develop")
+    depends_on("intel-oneapi-vtune", when="+vtune", type=("build", "link", "run"))
+    depends_on("caliper", when="+caliper", type=("build", "link", "run"))
+    depends_on("kokkos@4:", when="+tests", type=("build", "link", "run"))
     depends_on("kokkos@4:", when="+examples", type=("build", "link", "run"))
     depends_on("kokkos@4:+cuda", when="+nvtx", type=("build", "link", "run"))
     depends_on("kokkos@4:+rocm", when="+roctx", type=("build", "link", "run"))
-    depends_on('mpi', when='+mpi', type=("build", "link", "run"))
-    depends_on('papi@6:', when='+papi', type=("build", "link", "run"))
-    depends_on('variorum', when='+variorum" type=("build", "link", "run"))
+    depends_on("mpi", when="+mpi", type=("build", "link", "run"))
+    depends_on("papi@6:", when="+papi", type=("build", "link", "run"))
+    depends_on("variorum", when="+variorum", type=("build", "link", "run"))
 
-def build_env(self):
-    if '+nvtx' in spec:
-        setenv Kokkos_ENABLE_CUDA=1
+    def setup_build_environment(self, env):
+        # nvtx requires $Kokkos_ENABLE_CUDA to be set
+        if "+nvtx" in self.spec:
+            env.set("Kokkos_ENABLE_CUDA", "1")
 
-    if '+rocm' in spec:
-        setenv Kokkos_ENABLE_HIP=1
+        # roctx requires $Kokkos_ENABLE_HIP to be set
+        if "+roctx" in self.spec:
+            env.set("Kokkos_ENABLE_HIP", "1")
 
-    #if '+vtune' in spec:
-    #    setenv VTUNE_HOME spec['intel-oneapi-vtune'].prefix
-    #    ## Maybe leave this out?
+        # vtune requires VTUNE_HOME to be set
+        if "+vtune" in self.spec:
+            env.set("VTUNE_HOME", self.spec["vtune"].prefix)
 
-def cmake_args(self):
-    spec = self.spec
-    args = [
-        '-DKokkosTools_ENABLE_MPI=%s' % ('ON' if '+mpi' in spec else 'OFF'),
-        '-DKokkosTools_ENABLE_APEX=%s' % ('ON' if '+apex' in spec else 'OFF'),
-        '-DKokkosTools_ENABLE_CALIPER=%s' % ('ON' if '+caliper' in spec else 'OFF'),
-        '-DKokkosTools_ENABLE_EXAMPLES=%s' % ('ON' if '+examples' in spec else 'OFF'),
-        '-DKokkosTools_ENABLE_PAPI=%s' % ('ON' if '+papi' in spec else 'OFF'),
-        '-DKokkosTools_ENABLE_SINGLE=%s' % ('ON' if '+single' in spec else 'OFF'),
-        '-DKokkosTools_ENABLE_TESTS=%s' % ('ON' if '+tests' in spec else 'OFF')
-    ]
+    def cmake_args(self):
+        spec = self.spec
+        cmake_args = [
+            "-DKokkosTools_ENABLE_PAPI=%s" % ("ON" if "+papi" in spec else "OFF"),
+            "-DKokkosTools_ENABLE_MPI=%s" % ("ON" if "+mpi" in spec else "OFF"),
+            "-DKokkosTools_ENABLE_CALIPER=%s" % ("ON" if "+caliper" in spec else "OFF"),
+            "-DKokkosTools_ENABLE_APEX=%s" % ("ON" if "+apex" in spec else "OFF"),
+            "-DKokkosTools_ENABLE_EXAMPLES=%s" % ("ON" if "+examples" in spec else "OFF"),
+            "-DKokkosTools_ENABLE_TESTS=%s" % ("ON" if "+tests" in spec else "OFF"),
+            "-DKokkosTools_ENABLE_SINGLE=%s" % ("ON" if "+single" in spec else "OFF")
+        ]
 
-    if '+testing' or '+caliper' or '+examples' or '+nvtx' or '+roctx' or '+vtune' in spec:
-        args.append('-DKokkos_ROOT=%s' % spec['kokkos'].prefix)
+        if "+reuse-kokkos-compiler" in spec:
+            cmake_args.append("-DKokkosTools_REUSE_KOKKOS_COMPILER=%s" % "ON")
+        else:
+            cmake_args.append("-DKokkosTools_REUSE_KOKKOS_COMPILER=%s" % "OFF")
 
-    if '+cuda' in spec:
-        args.append('-DCMAKE_CXX_COMPILER=%s' % spec['kokkos'].kokkos_cxx)
+        if (
+            "+tests" or
+            "+caliper" or
+            "+examples" or
+            "+nvtx" or
+            "+roctx" or
+            "+vtune" in spec
+        ):
+            cmake_args.append("-DKokkos_ROOT=%s" % spec["kokkos"].prefix)
 
-    return args
+        if (
+            spec.satisfies("+nvtx")
+            and "^kokkos+cuda" in spec
+            and not "^kokkos%clang+cuda" in spec
+            or "^kokkos%cce+cuda"
+        ):
+            cmake_args.append("-DCMAKE_CXX_COMPILER=%s" %
+                               spec["kokkos-nvcc-wrapper"].kokkos_cxx)
+        elif (
+            "+nvtx" in spec
+            and "^kokkos+cuda"
+            and "^kokkos%clang+cuda"
+        ):
+            cmake_args.append("-DCMAKE_CXX_COMPILER=%s" % "{0}/bin/clang++".format(spec["clang"].prefix))
+        elif "+nvtx" in spec and "^kokkos%cce+cuda":
+            cmake_args.append("-DCMAKE_CXX_COMPILER=%s" % "{0}/bin/CC".format(spec["cce"].prefix))
+        elif "+roctx" in spec and "^kokkos+rocm":
+            cmake_args.append("-DCMAKE_CXX_COMPILER=%s" % spec["hip"].hipcc)
+        print("Debugging Cmake Args: %s", cmake_args)
+        return cmake_args
